d1594d4a14a3e0810371ed7a12dda62143768846
Paul van der Maden <paul@vandermadenconsultancy.nl>
2021-11-19 00:42:05 +0100

    updated for mattermost v6 support

--- sopelconf/modules/mattermostconnect.py
+++ sopelconf/modules/mattermostconnect.py
@@ -3,5 +3,5 @@ from sopel.module import event, rule
 @rule('.*')
 @event('001')
 def logon(bot, trigger):
-    bot.say("login {} {}".format(bot.config.mattermost.username, bot.config.mattermost.password), "mattermost")
+    bot.say("login {} {} {} {}".format(bot.config.mattermost.server, bot.config.mattermost.team, bot.config.mattermost.username, bot.config.mattermost.password), "mattermost")
 
